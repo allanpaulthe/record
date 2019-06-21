f22e39bace1b8aad8eb8292f542f7d4f1fccdb98
Alan paul <alanp@qburst.com>
2019-06-21 15:57:29 +0530

    first

---
+++ public/app/background.js
@@ -0,0 +1,9 @@
+// Called when the user clicks on the browser action
+/*global browser*/
+browser.browserAction.onClicked.addListener(function(tab) {
+   // Send a message to the active tab
+   browser.tabs.query({active: true, currentWindow:true},function(tabs) {
+        var activeTab = tabs[0];
+        browser.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+   });
+});
